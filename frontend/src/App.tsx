f3890b5a0471a4d25f6a9011821c73e39d314eee
Muhammad Shamil <muhammadshamil.2402@gmail.com>
2026-09-01 23:09:18 +0530

    Project structure initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        TutorFlow
+      </h1>
+    </main>
+  )
+}
+
+export default App
